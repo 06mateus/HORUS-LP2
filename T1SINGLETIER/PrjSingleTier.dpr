f38f286e1cc80af5e27325cc7a5befa8ea60cc07
Mateus Pilz <pilzmateus06@hotmail.com>
2016-08-12 00:45:34 -0300

    Feito quase toda interface

---
+++ T1SINGLETIER/PrjSingleTier.dpr
@@ -0,0 +1,14 @@
+program PrjSingleTier;
+
+uses
+  Vcl.Forms,
+  SingleTier in 'SingleTier.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
